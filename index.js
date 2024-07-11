54e53ef652fb9708a854c3d4814fd9c137a1ae24
victorndungi <victor.ndungi@student.moringaschool.com>
2024-07-11 09:55:33 +0300

    new arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+// Write your code h
+
+// Define num1 and num2 such that their product is 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 such that the remainder of num3 divided by num4 is 4
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Find the maximum number in a set that includes 20
+const max = Math.max(1, 5, 10, 15, 20);
+
+
